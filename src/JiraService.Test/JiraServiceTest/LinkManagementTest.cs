842f28f834db779775094377ef083dfafd7b8d9a
Tajik Mohammad <M.tajik@Tosan.com>
2022-02-05 17:48:42 +0330

    seperate test files in better categories

---
+++ src/JiraService.Test/JiraServiceTest/LinkManagementTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Atlassian.Jira.Remote;
+using JiraService.Test.Model;
+using JiraService.Test.Service;
+using JiraService.Consts;
+using JiraService.Contracts;
+using JiraService.Extensions;
+using JiraService.JiraFields;
+using JiraService.Models;
+using Xunit;
+
+namespace JiraService.Test.JiraServiceTest
+{
+    public class LinkManagementTest
+    {
+        [Theory]
+        [ClassData(typeof(IssuesDataset))]
+        public async void GetLink_jiraService(IJiraService jiraService, string key)
+        {
+            var links = await jiraService.LinkManagement.GetIssueLinks(key);
+            Assert.All(links, a =>
+            {
+                Assert.NotNull(a.LinkType);
+                Assert.NotNull(a.InwardIssueKey);
+                Assert.NotNull(a.OutwardIssueKey);
+            });
+        }
+
+        [Theory]
+        [ClassData(typeof(IssuesDataset))]
+        public async void GetLinks_jiraService(IJiraService jiraService, string key)
+        {
+            var issues = await jiraService.LinkManagement.GetLinks<IssueModel>(key, JiraLinkTypes.Cloners);
+            Assert.All(issues, a =>
+            {
+                Assert.NotNull(a.Key);
+                Assert.NotNull(a.Type);
+                Assert.NotNull(a.ProjectKey);
+            });
+        }
+
+        [Theory]
+        [ClassData(typeof(IssuesDataset))]
+        public async void Link_jiraService(IJiraService jiraService, string key)
+        {
+            var newkey = await jiraService.Issue.Create(ChangeIssueInitializer.ChangeIssue.AddCustomFields());
+            var exec = await jiraService.LinkManagement.Link(key, newkey, JiraLinkTypes.Duplicate);
+            Assert.True(exec);
+            await DeleteIssue(jiraService, newkey);
+        }
+
+        private static async Task DeleteIssue(IJiraService jiraService, string key)
+        {
+            await jiraService.Issue.Delete(key);
+            var exception = await Assert.ThrowsAsync<ResourceNotFoundException>(
+                async () => await jiraService.Issue.Get<ChangeIssue>(key));
+            Assert.Contains("Issue Does Not Exist", exception.Message);
+        }
+    }
+}
---
+++ src/JiraService.Test/JiraServiceTest/ModelTest.cs
@@ -0,0 +1,84 @@
+using System.Linq;
+using Atlassian.Jira;
+using JiraService.Test.Model;
+using JiraService.Test.Service;
+using JiraService.Contracts;
+using JiraService.JiraFields;
+using JiraService.Models;
+using JiraService.Services;
+using Xunit;
+
+namespace JiraService.Test.JiraServiceTest
+{
+
+    public class ModelTest
+    {
+        [Fact]
+        public void CheckValidation_CreateIssueModel()
+        {
+            var issue = new IssueModel();
+            Assert.True(!issue.IsValid(out var validates));
+            var validateMessages = validates as ValidateMessage[] ?? validates.ToArray();
+            Assert.Contains(validateMessages, valid => valid.Message.Contains(nameof(issue.ProjectKey)));
+            Assert.Contains(validateMessages, valid => valid.Message.Contains(nameof(issue.Type)));
+            issue.ProjectKey = "test";
+            issue.Type = "test";
+            Assert.True(issue.IsValid(out validates));
+            Assert.True(!validates.ToList().Any());
+        }
+
+        [Fact]
+        public void GetSchema_CreateIssueModel()
+        {
+            var issue = new ChangeIssue();
+            var schema = issue.GetCustomFields();
+            Assert.True(schema.ContainsKey("Ticket Summary"));
+        }
+
+        [Fact]
+        public void CheckTypeSafeAndDictionary_CreateIssueModel()
+        {
+            var changeTs = ChangeIssueInitializer.ChangeIssue.AddTypeSafeCustomFields();
+            var change = ChangeIssueInitializer.ChangeIssue.AddCustomFields();
+            var changeFields = change.GetCustomFields();
+            changeTs.GetCustomFields().ToList().ForEach(f => { Assert.Equal(changeFields[f.Key], f.Value); });
+
+            Assert.Equal(changeTs.BusinessService, change.BusinessService);
+            Assert.Equal(changeTs.Customer, change.Customer);
+            Assert.Equal(changeTs.PrimaryCustomer, change.PrimaryCustomer);
+            Assert.Equal(changeTs.ChangeType, change.ChangeType);
+        }
+
+        [Fact]
+        public void AssignCustomField_CheckCustomFieldAttribute_CreateIssueModel()
+        {
+            var change = new ChangeIssue();
+            change.Customer = new InsightJField
+                {Value = new InsightField {Id = 265, Key = "CRM-265", Name = "Amal Bank"}};
+            Assert.Null(change.Customer.Attribute);
+            change.SetAttributeInCustomFields();
+            Assert.NotNull(change.Customer.Attribute);
+        }
+
+        [Theory]
+        [ClassData(typeof(JiraProvider))]
+        public void Map_IssueModel_JiraIssue(Jira jiraService)
+        {
+            var issueModel = new IssueModel();
+            issueModel.ProjectKey = "SRV";
+            var issue = jiraService.CreateIssue(issueModel.ProjectKey);
+            issue = IssueMapper.Map(issue, issueModel);
+            Assert.True(issue != null);
+        }
+
+        [Theory]
+        [ClassData(typeof(IssuesDataset))]
+        public async void Map_JiraIssueToIsseModel(IJiraService jiraService, string key)
+        {
+            var changeIssue = await jiraService.Issue.Get<ChangeIssue>(key);
+            Assert.NotNull(changeIssue.BusinessService.Attribute);
+            Assert.NotNull(changeIssue.ChangeType);
+            Assert.NotNull(changeIssue.Customer.Value);
+        }
+    }
+}
