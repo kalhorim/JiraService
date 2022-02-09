fe3202126a134da75832dfa358437b1bba68abd0
Tajik Mohammad <M.tajik@Tosan.com>
2022-02-09 13:11:17 +0330

    #12 Test Custom Jira Link

---
+++ src/JiraService.Test/Model/CustomLinkTypes.cs
@@ -0,0 +1,19 @@
+﻿using JiraService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraService.Test.Model
+{
+    internal static class CustomLinkTypes
+    {
+        public static JiraLinkType AdditionalCustomer => new JiraLinkType
+        {
+            Name = "Additional Customer",
+            InwardDescription = "Additional Customer issue",
+            OutwardDescription = "Primary customer issue"
+        };
+    }
+}
