040681209bb7dad848cd249b4631a6368a4e7bed
Tajik Mohammad <M.tajik@Tosan.com>
2022-02-13 13:10:04 +0330

    Implement ArrayJField #21

--- src/JiraService/JiraFields/ArrayJField.cs
+++ src/JiraService/JiraFields/ArrayJField.cs
@@ -4,8 +4,10 @@ namespace JiraService.JiraFields
 {
     public class ArrayJField : JiraCustomFieldBase
     {
-        protected internal override string GetJiraValue => throw new NotImplementedException();
+        public string[] Values { get; set; }
 
-        protected internal override string[] SetJiraValue { set => throw new NotImplementedException(); }
+        protected internal override string GetJiraValue => Values == null ? null : string.Join(", ", Values);
+
+        protected internal override string[] SetJiraValue { set => Values = value; }
     }
 }
